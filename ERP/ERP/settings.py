97872dde7aab8e4d8bc4f74b22d598cc63da9175
willys7 <wilfuentes0320@gmail.com>
2018-03-20 09:40:22 -0600

    add user models with out migrations

--- ERP/ERP/settings.py
+++ ERP/ERP/settings.py
@@ -75,9 +75,17 @@ WSGI_APPLICATION = 'ERP.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'ERP_Ferre',
+        'USER': 'postgres',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
--- ERP/Users/models.py
+++ ERP/Users/models.py
@@ -4,3 +4,55 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+from django.db import models
+from django.conf import settings
+import uuid
+import datetime
+
+#Handle User model 
+class UserManager(models.Manager):
+    print("SII")
+
+#Handle model LogUser model
+class LogUserManager(models.Manager):
+    print("SII")
+
+#Handle model Token model
+class AuthTokenManager(models.Manager):
+    print("SII")
+
+#Handle model Role model
+class RoleManager(models.Manager):
+    print("SII")
+
+#Create User model
+class User(models.Model):
+    name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    user_name = models.CharField(max_length=240, unique=True)
+    password = models.CharField(max_length=50)
+    email = models.EmailField(verbose_name='email address', max_length=254, unique=True)
+    objects = UserManager()
+    def __str__(self):
+        return self.user_name
+
+#Create Log_User model
+class LogUser(models.Model):
+    user_name = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateField(auto_now=True)
+    objects = LogUserManager()
+
+#Create Token model
+class Token(models.Model):
+    token = models.CharField(max_length=254, unique=True)
+    date_created = models.DateField(auto_now=True)
+    last_activation = models.DateTimeField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    objects = AuthTokenManager()
+
+#Create Role  model
+class Role(models.Model):
+    name = models.CharField(max_length=30)
+    date = models.DateField(auto_now=True)
+    objects = RoleManager()
+    
